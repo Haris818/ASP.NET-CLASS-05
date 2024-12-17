b7672202df6a7bbe2a39bd119431915f10dd84c4
nabihakhan34 <biyakhan551@gmail.com>
2024-12-17 16:52:12 +0500

    Add project files.

---
+++ CLASS 05/Controllers/HOMEController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CLASS_05.Controllers
+{
+    public class HOMEController : Controller
+    {
+        // GET: HOME
+        public ActionResult HOME()
+        {
+            ViewData["Var1"] = "Data comes from ViewData";
+            ViewBag.Var2 = "Data comes from ViewBag";
+            TempData["Var3"] = "Data comes from TempData";
+            Session["Var4"] = "Data comes from Session";
+
+            return View();
+        }
+
+        public ActionResult ABOUT()
+        {
+            TempData["var2"] = "data come from view beg";
+            return View();
+        }
+
+
+        public ActionResult CONTACT()
+        {
+            return View();
+        }
+    }
+}
